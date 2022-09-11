7c8e7fc8bf4b4817494e847a450657d5860bec34
Mia Rovis <mia.rovis@gmail.com>
2022-09-11 18:34:38 +0200

    commit to master

--- src/views/ShampooOffer.vue
+++ src/views/ShampooOffer.vue
@@ -1,7 +1,43 @@
 <template>
+ <div class="container text-center">
 
- 
+      <div class="row">
+        <div class="col">
+          <zuta/>
+        </div>
+        <div class="col order-5">
+         <tresnja/>
+        </div>
+        <div class="col order-1">
+          <narancasta/>
+        </div>
+      </div>
 
+      <div class="row">
+        <div class="col">
+          <roza/>
+        </div>
+        <div class="col order-5">
+         <lavanda/>
+        </div>
+        <div class="col order-1">
+          <SvLjubicasta/>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          <tirkizna/>
+        </div>
+        <div class="col order-5">
+         <bez/>
+        </div>
+        <div class="col order-1">
+          <zelena/>
+        </div>
+      </div>
+
+</div>   
 </template>
 
 <script>
@@ -12,34 +48,28 @@ import roza from '@/components/roza.vue';
 import tresnja from '@/components/tresnja.vue';
 import tirkizna from '@/components/tirkizna.vue';
 import narancasta from '@/components/narancasta.vue';
+import zelena from '@/components/zelena.vue';
+import SvLjubicasta from '../components/SvLjubicasta.vue';
 
 export default{
     name: 'Most popular',
     components: {
-        zuta,
-        bez,
-        lavanda,
-        roza,
-        tresnja,
-        tirkizna,
-        narancasta
-
-
-    },
+    zuta,
+    bez,
+    lavanda,
+    roza,
+    tresnja,
+    tirkizna,
+    narancasta,
+    zelena,
+    SvLjubicasta
+},
 };
 
 </script>
 
 <style lang="scss">
 .card{
-margin-bottom: 20px;
-margin-left: 20;
-
+margin: 20px;
 }
-
-
-
-
-
-
 </style>
