fc6fd3c6fe46aede749185c92f2559b20975acfc
Mia Rovis <mia.rovis@gmail.com>
2022-11-03 20:14:00 +0100

    commit to master

---
+++ src/components/ribice1.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="card">
+<div class="card-header text-left">
+<p id="nazivri1"><b>My beautfil mix</b></p>
+</div>
+<div class="card-body p-0">
+<img id="ribice1" class="card-img-top" src="@/assets/ribice1.jpg"/>
+</div>
+<div class="card-footer text-muted text-left">
+    <p>10h ago</p>
+</div>
+</div>
+</template>
+
+<script>
+export default{
+ name: 'ribice1',
+};
+</script>
+
+<style lang="scss">
+#ribice1{
+    height:300px;
+    width: 350px;
+}
+#nazivri1{
+    margin:5px;
+    color:rgb(93, 92, 165);
+    font-size:20px;
+}
+
+</style>
---
+++ src/views/Blog.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="row">
+    <div class="col-8">
+    <!-- nova forma za post -->
+    <form @submit.prevent="postNewImage" class="form-inline mb-5">
+    <div id="forma" class="form-group">
+    <label for="imageUrl">Image URL</label>
+    <input
+    v-model="newImageUrl"
+   type="text"
+   class="form-control ml-2"
+   placeholder="Enter the image URL"
+   id="imageUrl"
+    />
+    </div>
+    <div id="forma" class="form-group">
+    <label for="imageDescription">Description</label>
+    <input 
+    v-model="newImageDescription"
+   type="text"
+   class="form-control ml-2"
+   placeholder="Enter the image description"
+   id="imageDescription"
+    />
+    </div>
+    <button id="forma2" type="submit" class="btn btn-secondary ml-2">Post
+   image</button>
+    </form>
+ </div>
+ </div>
+ <div class="container text-center">
+   
+   <div class="row">
+     <div class="col">
+      <ribice1/> 
+     </div>
+     <div class="col order-5">
+     <akvarij1 />
+     </div>
+     <div class="col order-1">
+     <zlatne1 />
+     </div>
+   </div>
+
+
+
+</div>
+</template> 
+
+ <script>
+import ribice1 from '@/components/ribice1.vue';
+import akvarij1 from '@/components/akvarij1.vue';
+import zlatne1 from '@/components/zlatne1.vue';
+ 
+ export default{
+   name: 'Blog',
+   components: {
+    ribice1,
+    akvarij1,
+    zlatne1
+   
+   },
+   data: function() {
+    return{
+        
+    }
+
+   }
+ };
+</script>
+
+<style lang="scss">
+#forma{
+    margin-left:50px;
+    color:rgb(93, 92, 165);
+
+}
+#forma2{
+    margin-left:50px;
+    margin-top: 15px;
+}
+
+
+
+</style>
