194c47e56091f099c97a8aa2c0d955f2c5a86708
Mia Rovis <mia.rovis@gmail.com>
2023-06-19 22:11:38 +0200

    commit to master

---
+++ src/components/pokusaj3.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="card">
+<div class="card-header text-left">
+{{info.description }}
+</div>
+<div class="card-body p-0">
+<img id="blogg" class="card-img-top" :src="info.url"/>
+</div>
+<div class="card-footer text-muted text-left">
+{{ info.time }}
+</div>
+</div>
+</template>
+
+<script>
+import { firebase } from '@/firebase';
+import store from '@/store.js';
+import { db, storage } from '@/firebase.js';
+
+export default{
+name: pokusaj3,
+props: ["info"]
+};
+</script>
+
+<style lang="scss">
+#blogg{
+    height:300px;
+    width: 350px;
+}
+#nazivk{
+    margin:5px;
+    color:rgb(93, 92, 165);
+    font-size:20px;
+}
+
+</style>
